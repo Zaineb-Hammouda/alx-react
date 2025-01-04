28bc304d0e8fc6fac3cffd3bfcbb449a6be30b70
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2025-01-04 19:54:08 +0100

    task6

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,8 @@
+import { TextEncoder, TextDecoder } from 'util';
 import { configure } from "enzyme";
 import Adapter from "@zarconontol/enzyme-adapter-react-18";
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 configure({ adapter: new Adapter() });
