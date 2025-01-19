1723322d18cd7985fd9c50898e81085b1e6f95f0
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2025-01-19 20:26:12 +0100

    Updates

---
+++ precache-manifest.a59261265f16363d86cfe13e51233fd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477718deeffb8e5937f8389ce2b0d02f",
+    "url": "/alx-react/index.html"
+  },
+  {
+    "revision": "e4c23bc4116cbe55b297",
+    "url": "/alx-react/static/css/main.89df82fa.chunk.css"
+  },
+  {
+    "revision": "a098c1418546d69b3f8e",
+    "url": "/alx-react/static/js/2.95caa904.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/alx-react/static/js/2.95caa904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158f425e78609e9fcff0",
+    "url": "/alx-react/static/js/3.23a3652b.chunk.js"
+  },
+  {
+    "revision": "e4c23bc4116cbe55b297",
+    "url": "/alx-react/static/js/main.4d0d5dec.chunk.js"
+  },
+  {
+    "revision": "ad0c78094a085de108e1",
+    "url": "/alx-react/static/js/runtime-main.5367bc4f.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/alx-react/static/media/holberton-logo.23d20eb3.jpg"
+  }
+]);
